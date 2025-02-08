472d079b179a66f6fcd9c1e4981787cc6e120fca
tbingle <sandmanthomas9@icloud.com>
2025-02-08 11:20:42 -0500

    Task 2 - Order Details

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,18 @@ let customer = {
 console.log (`Name: ${customer.Name}`);
 console.log (`Age: ${customer.Age}`);
 console.log (`Email:${customer.Email}`);
+
+//Task 2 
+//Creating order identification
+let order = {
+    orderID: 3456,
+    totalAmount: 234,
+    status: "Processing",
+    displayOrder: function () {
+        console.log(`Order ID: ${this.orderID}`)
+        console.log(`Total Amount : $${this.totalAmount}`)
+        console.log(`Status: ${this.status}`)
+    }
+};
+order.displayOrder(); //Show order details in console
+
